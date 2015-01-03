bcd2a582fcc22cb65b0db3dd8c0549809c7ee613
Cristiano Veloz <ctoveloz@gmail.com>
2015-01-03 03:51:21 -0300

    make it better

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Cristiano Matos'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '79db072ade1c0860f948c5474c657baa74fc1375',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
